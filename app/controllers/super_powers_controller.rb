4d645477fb1b3e5c17aa758a883667f40ff5c4ee
kevin ly <kev3010@outlook.fr>
2023-06-20 12:30:23 +0200

    add dotenv-rails

--- app/controllers/super_powers_controller.rb
+++ app/controllers/super_powers_controller.rb
@@ -8,17 +8,30 @@ class SuperPowersController < ApplicationController
   end
 
   def new
+    @super_power = SuperPower.new
   end
 
   def create
+    @super_power = SuperPower.new(super_power_params)
+    @super_power.save!
+    redirect_to super_power_path
   end
 
   def edit
+    @super_power = SuperPower.find(params[:id])
   end
 
   def update
+    @super_power = SuperPower.find(params[:id])
+    @super_power.update(super_power_params)
   end
 
   def destroy
+  end
+
+  private
+
+  def super_power_params
+    params.require(:super_power).permit(:name, :description, :price, :poster_url)
   end
 end
