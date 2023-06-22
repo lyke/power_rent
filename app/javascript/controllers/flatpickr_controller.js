3f04c9c24250be88e8becff37f0d210a0882d6f4
Anastasiia Deriabina <anastasiia.deriabina@gmail.com>
2023-06-22 17:42:38 +0200

    new calendar

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -7,10 +7,11 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 export default class extends Controller {
   static targets = [ "startTime", "endTime" ]
   connect() {
+    // console.log('slt')
     flatpickr(this.startTimeTarget, {
               enableTime: true,
               // Provide an id for the plugin to work
-              plugins: [new rangePlugin({ input: "#end_time", input: "#secondRangeInput"})]})
+              plugins: [new rangePlugin({ input: "#end_time"})]})
     flatpickr(this.endTimeTarget, {})
   }
 }
